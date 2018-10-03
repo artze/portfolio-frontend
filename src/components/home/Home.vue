9c1c3f146c63b4dd5220ed490a0c0cc16cc1755f
artze <arthurwtm@gmail.com>
2018-10-04 00:19:46 +0200

    Adjust project cards based on breakpoint

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -41,28 +41,28 @@
                 <v-layout justify-center>
                     <v-flex lg8>
                         <v-layout justify-start wrap>
-                            <v-flex lg4>
+                            <v-flex sm6 md4 lg4>
                                 <v-card class="project-card mt-4">
                                     <v-card-title primary-title class="justify-center">
                                         <router-link to="/quote-machine" tag="h3" class="project-card-title-text">Quote Machine</router-link>
                                     </v-card-title>
                                 </v-card>
                             </v-flex>
-                            <v-flex lg4>
+                            <v-flex sm6 md4 lg4>
                                 <v-card class="project-card mt-4">
                                     <v-card-title primary-title class="justify-center">
                                         <router-link to="/wiki-viewer" tag="h3" class="project-card-title-text">Wikipedia Viewer</router-link>
                                     </v-card-title>
                                 </v-card>
                             </v-flex>
-                            <v-flex lg4>
+                            <v-flex sm6 md4 lg4>
                                 <v-card class="project-card mt-4">
                                     <v-card-title primary-title class="justify-center">
                                         <router-link to="/journey-tracker" tag="h3" class="project-card-title-text">Journey Tracker</router-link>
                                     </v-card-title>
                                 </v-card>
                             </v-flex>
-                            <v-flex lg4>
+                            <v-flex sm6 md4 lg4>
                                 <v-card class="project-card mt-4">
                                     <v-card-title primary-title class="justify-center">
                                         <router-link to="/chat-app" tag="h3" class="project-card-title-text">Chat App</router-link>
