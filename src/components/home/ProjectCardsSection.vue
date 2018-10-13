8040235a445ce50366607a86cdfdee35e357bca6
artze <arthurwtm@gmail.com>
2018-10-13 10:50:28 +0200

    Restyle project cards

--- src/components/home/ProjectCardsSection.vue
+++ src/components/home/ProjectCardsSection.vue
@@ -3,44 +3,44 @@
         <v-flex xs8 sm8 md8 lg8>
             <v-layout justify-start wrap>
                 <v-flex xs12 sm6 md4 lg4>
-                    <v-card class="project-card mt-4">
+                    <router-link to="/journey-tracker" tag="div" class="project-card mt-4">
                         <v-card-title primary-title class="justify-center">
-                            <router-link to="/journey-tracker" tag="h3" class="project-card-title-text">Journey Tracker</router-link>
+                            <h3 class="project-card-title-text">Journey Tracker</h3>
                         </v-card-title>
                         <v-card-text class="project-card-caption">
                             A visualization tool to view tracked journeys.
                         </v-card-text>
-                    </v-card>
+                    </router-link>
                 </v-flex>
                 <v-flex xs12 sm6 md4 lg4>
-                    <v-card class="project-card mt-4">
+                    <router-link to="/chat-app" tag="div" class="project-card mt-4">
                         <v-card-title primary-title class="justify-center">
-                            <router-link to="/chat-app" tag="h3" class="project-card-title-text">Chat App</router-link>
+                            <h3 class="project-card-title-text">Chat App</h3>
                         </v-card-title>
                         <v-card-text class="project-card-caption">
                             A super simple chat app created with Websockets.
                         </v-card-text>
-                    </v-card>
+                    </router-link>
                 </v-flex>
                 <v-flex xs12 sm6 md4 lg4>
-                    <v-card class="project-card mt-4">
+                    <router-link to="/quote-machine" tag="div" class="project-card mt-4">
                         <v-card-title primary-title class="justify-center">
-                            <router-link to="/quote-machine" tag="h3" class="project-card-title-text">Quote Machine</router-link>
+                            <h3 class="project-card-title-text">Quote Machine</h3>
                         </v-card-title>
                         <v-card-text class="project-card-caption">
                             Generate random quotes from notable figures.
                         </v-card-text>
-                    </v-card>
+                    </router-link>
                 </v-flex>
                 <v-flex xs12 sm6 md4 lg4>
-                    <v-card class="project-card mt-4">
+                    <router-link to="/wiki-viewer" tag="div" class="project-card mt-4">
                         <v-card-title primary-title class="justify-center">
-                            <router-link to="/wiki-viewer" tag="h3" class="project-card-title-text">Wikipedia Viewer</router-link>
+                            <h3 class="project-card-title-text">Wikipedia Viewer</h3>
                         </v-card-title>
                         <v-card-text class="project-card-caption">
                             Search Wikipedia entries with this handy tool.
                         </v-card-text>
-                    </v-card>
+                    </router-link>
                 </v-flex>
             </v-layout>
         </v-flex>
@@ -55,21 +55,21 @@
     margin: 0px 15px;
 }
 
+.project-card:hover {
+    cursor: pointer;
+    box-shadow: 0px 0px 0px 0.7px #69ffdf inset;
+}
+
 .project-card-title-text {
     font-family: 'Raleway';
     font-size: 1.1em;
     font-weight: 400;
 }
 
-.project-card-title-text:hover {
-    text-decoration: underline;
-    cursor: pointer;
-}
-
 .project-card-caption {
     font-size: 0.95em;
     color: #757575;
-    padding-top: 0.5em;
+    padding-top: 0.1em;
     text-align: center;
 }
 </style>
