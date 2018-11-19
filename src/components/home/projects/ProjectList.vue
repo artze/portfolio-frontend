9eb952d354be4e50eb98f15c4130d5e9f6949f88
artze <arthurwtm@gmail.com>
2018-11-19 21:55:25 +0100

    Add screenshot pictures for hobby projects

---
+++ src/components/home/projects/ProjectList.vue
@@ -0,0 +1,344 @@
+<template>
+    <v-layout justify-center>
+        <v-flex xs10 sm10 md10 lg10>
+            <v-layout class="project-card-layout-section" justify-space-between>
+                <v-flex xs12 sm12 md4 lg4>
+                    <v-layout fill-height align-center>
+                        <div class="project-card">
+                            <h3 class="project-card-title-text">Productivity Tools</h3>
+                            <p class="project-card-text">
+                                A suite of Web Applications designed to streamline operational processes in my company.
+                            </p>
+                            <hr>
+                            <p class="project-card-text points">
+                                <span class="bold">Tech Stack:</span>
+                                MySql, Nodejs, Vuejs
+                            </p>
+                            <p class="project-card-text points">
+                                <span class="bold">Role:</span>
+                                Designed solution and develop product
+                            </p>
+                            <p class="project-card-text points">
+                                <span class="bold">Team:</span>
+                                Myself
+                            </p>
+                            <v-layout justify-center>
+                                <div @click="goToProductivityToolsPage" class="project-card-link">
+                                    Read More
+                                </div>
+                            </v-layout>
+                        </div>
+                    </v-layout>
+                </v-flex>
+                <v-flex xs12 sm12 md8 lg8>
+                    <img class="project-img" src="../../../assets/images/ProductivityToolScreenShot.png">
+                </v-flex>
+            </v-layout>
+            <v-layout class="project-card-layout-section" justify-space-between>
+                <v-flex xs12 sm12 md4 lg4>
+                    <v-layout fill-height align-center>
+                        <div class="project-card">
+                            <h3 class="project-card-title-text">IoT Platform</h3>
+                            <p class="project-card-text">
+                                A platform that allows users to manage, maintain, and get live data from IoT devices.
+                            </p>
+                            <hr>
+                            <p class="project-card-text points">
+                                <span class="bold">Tech Stack:</span>
+                                MySql, Couchbase, OpenNATS, Java, Spring, Vaadin
+                            </p>
+                            <p class="project-card-text points">
+                                <span class="bold">Role:</span>
+                                Software Developer
+                            </p>
+                            <p class="project-card-text points">
+                                <span class="bold">Team:</span>
+                                3 Developers including myself
+                            </p>
+                            <v-layout justify-center>
+                                <div @click="goToMicroservicesPage" class="project-card-link">
+                                    Read More
+                                </div>
+                            </v-layout>
+                        </div>
+                    </v-layout>
+                </v-flex>
+                <v-flex xs12 sm12 md8 lg8>
+                    <img class="project-img" src="../../../assets/images/d2sScreenShot.png">
+                </v-flex>
+            </v-layout>
+            <v-layout class="project-card-layout-section" justify-space-between>
+                <v-flex xs12 sm12 md4 lg4>
+                    <v-layout fill-height align-center>
+                        <div class="project-card">
+                            <h3 class="project-card-title-text">Journey Tracker</h3>
+                            <p class="project-card-text">
+                                A visualization tool to view tracked journeys.
+                            </p>
+                            <hr>
+                            <p class="project-card-text points">
+                                <span class="bold">Tech Stack:</span>
+                                MySql, Nodejs, Vuejs
+                            </p>
+                            <p class="project-card-text points">
+                                <span class="bold">Role:</span>
+                                Developer
+                            </p>
+                            <p class="project-card-text points">
+                                <span class="bold">Team:</span>
+                                Myself
+                            </p>
+                            <v-layout justify-center>
+                                <div @click="goToJourneyTracker" class="project-card-link">
+                                    Go to App
+                                </div>
+                            </v-layout>
+                        </div>
+                    </v-layout>
+                </v-flex>
+                <v-flex xs12 sm12 md8 lg8>
+                    <img class="project-img" src="../../../assets/images/JourneyTrackerScreenShot.png">
+                </v-flex>
+            </v-layout>
+            <v-layout class="project-card-layout-section" justify-space-between>
+                <v-flex xs12 sm12 md4 lg4>
+                    <v-layout fill-height align-center>
+                        <div class="project-card">
+                            <h3 class="project-card-title-text">Chat App</h3>
+                            <p class="project-card-text">
+                                A super simple chat app built with websockets
+                            </p>
+                            <hr>
+                            <p class="project-card-text points">
+                                <span class="bold">Tech Stack:</span>
+                                Nodejs, Vuejs
+                            </p>
+                            <p class="project-card-text points">
+                                <span class="bold">Role:</span>
+                                Developer
+                            </p>
+                            <p class="project-card-text points">
+                                <span class="bold">Team:</span>
+                                Myself
+                            </p>
+                            <v-layout justify-center>
+                                <div @click="goToChatApp" class="project-card-link">
+                                    Go to App
+                                </div>
+                            </v-layout>
+                        </div>
+                    </v-layout>
+                </v-flex>
+                <v-flex xs12 sm12 md8 lg8>
+                    <img class="project-img" src="../../../assets/images/ChatAppScreenShot.png">
+                </v-flex>
+            </v-layout>
+            <v-layout class="project-card-layout-section" justify-space-between>
+                <v-flex xs12 sm12 md4 lg4>
+                    <v-layout fill-height align-center>
+                        <div class="project-card">
+                            <h3 class="project-card-title-text">Quote Machine</h3>
+                            <p class="project-card-text">
+                                Generate random quotes from notable figures.
+                            </p>
+                            <hr>
+                            <p class="project-card-text points">
+                                <span class="bold">Tech Stack:</span>
+                                Vuejs
+                            </p>
+                            <p class="project-card-text points">
+                                <span class="bold">Role:</span>
+                                Developer
+                            </p>
+                            <p class="project-card-text points">
+                                <span class="bold">Team:</span>
+                                Myself
+                            </p>
+                            <v-layout justify-center>
+                                <div @click="goToQuoteMachine" class="project-card-link">
+                                    Go to App
+                                </div>
+                            </v-layout>
+                        </div>
+                    </v-layout>
+                </v-flex>
+                <v-flex xs12 sm12 md8 lg8>
+                    <img class="project-img" src="../../../assets/images/QuoteMachineScreenShot.png">
+                </v-flex>
+            </v-layout>
+            <v-layout class="project-card-layout-section" justify-space-between>
+                <v-flex xs12 sm12 md4 lg4>
+                    <v-layout fill-height align-center>
+                        <div class="project-card">
+                            <h3 class="project-card-title-text">Wikipedia Viewer</h3>
+                            <p class="project-card-text">
+                                Search Wikipedia entries with this handy tool.
+                            </p>
+                            <hr>
+                            <p class="project-card-text points">
+                                <span class="bold">Tech Stack:</span>
+                                Vuejs
+                            </p>
+                            <p class="project-card-text points">
+                                <span class="bold">Role:</span>
+                                Developer
+                            </p>
+                            <p class="project-card-text points">
+                                <span class="bold">Team:</span>
+                                Myself
+                            </p>
+                            <v-layout justify-center>
+                                <div @click="goToWikiViewer" class="project-card-link">
+                                    Go to App
+                                </div>
+                            </v-layout>
+                        </div>
+                    </v-layout>
+                </v-flex>
+                <v-flex xs12 sm12 md8 lg8>
+                    <img class="project-img" src="../../../assets/images/WikiViewerScreenShot.png">
+                </v-flex>
+            </v-layout>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+export default {
+    methods: {
+        goToProductivityToolsPage() {
+            let routeData = this.$router.resolve({
+                path: '/projects/productivity-tools'
+            })
+            window.open(routeData.href, '_blank')
+        },
+        goToMicroservicesPage() {
+            let routeData = this.$router.resolve({
+                path: '/projects/microservices'
+            })
+            window.open(routeData.href, '_blank')
+        },
+        goToJourneyTracker() {
+            let routeData = this.$router.resolve({
+                path: '/journey-tracker'
+            })
+            window.open(routeData.href, '_blank')
+        },
+        goToChatApp() {
+            let routeData = this.$router.resolve({
+                path: '/chat-app'
+            })
+            window.open(routeData.href, '_blank')
+        },
+        goToQuoteMachine() {
+            let routeData = this.$router.resolve({
+                path: '/quote-machine'
+            })
+            window.open(routeData.href, '_blank')
+        },
+        goToWikiViewer() {
+            let routeData = this.$router.resolve({
+                path: '/wiki-viewer'
+            })
+            window.open(routeData.href, '_blank')
+        }
+    }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Raleway:400,600,800');
+
+.project-img {
+    box-shadow: 0 5px 8px 0 rgba(0,0,0,0.2);
+    width: 800px;
+}
+
+.project-card-layout-section {
+    margin-top: 100px;
+}
+
+.project-card {
+    min-height: 28em;
+    padding: 30px;
+    transition: box-shadow 0.2s ease-in, transform 0.2s ease-in;
+}
+
+.project-card hr {
+    height: 1px;
+    background-color: #b3b3b3;
+    border: none;
+    margin: 1.5em 0;
+}
+
+.project-card-title-text {
+    font-family: 'Raleway';
+    font-size: 2em;
+    font-weight: 400;
+}
+
+.project-card-text {
+    font-family: 'Roboto';
+    font-size: 1.4em;
+    font-weight: 200;
+    color: #3a3a3a;
+    padding-top: 0.1em;
+    text-align: left;
+}
+
+.project-card-text.points {
+    font-size: 1.2em;
+}
+
+.project-card-text.points .bold {
+    font-weight: 500;
+}
+
+.project-card-link {
+    box-shadow: 0 0 0 2px #1e4cff;
+    margin-top: 1em;
+    padding: 10px 20px;
+    transition: box-shadow 0.2s ease-in, transform 0.2s ease-in;
+    font-family: 'Raleway';
+    font-weight: 600;
+    color: #1e4cff;
+}
+
+.project-card-link:hover {
+    cursor: pointer;
+    box-shadow: 0.5em 0.5em 0 0 #69ffdf, 0 0 0 1px #69ffdf inset;
+    transform: translate(-2px, -2px);
+    color: #000000;
+}
+
+@media only screen and (max-width : 600px) {
+    .project-card {
+        margin: 15px 0px;
+        min-height: 10em;
+    }
+
+    .project-card-title-text {
+        font-size: 1.4em;
+    }
+
+    .project-card-text {
+        font-size: 1em;
+    }
+}
+
+@media only screen and (min-width : 600px) and (max-width : 960px) {
+    .project-card {
+        margin: 15px 0px;
+        min-height: 14em;
+    }
+
+    .project-card-title-text {
+        font-size: 1.7em;
+    }
+
+    .project-card-text {
+        font-size: 1.3em;
+    }
+}
+
+</style>
--- src/components/home/projects/Projects.vue
+++ src/components/home/projects/Projects.vue
@@ -3,8 +3,7 @@
         <v-flex xs12 sm12 md12 lg12>
             <v-layout column justify-center style="height: 100%;">
                 <v-flex xs1 sm1 md1 lg1>
-                    <work-project-cards></work-project-cards>
-                    <hobby-project-cards></hobby-project-cards>
+                    <project-list></project-list>
                 </v-flex>
             </v-layout>
         </v-flex>
@@ -12,13 +11,11 @@
 </template>
 
 <script>
-import WorkProjectCards from './WorkProjectCards'
-import HobbyProjectCards from './HobbyProjectCards'
+import ProjectList from './ProjectList'
 
 export default {
     components: {
-        'work-project-cards': WorkProjectCards,
-        'hobby-project-cards': HobbyProjectCards
+        'project-list': ProjectList
     }
 }
 </script>
