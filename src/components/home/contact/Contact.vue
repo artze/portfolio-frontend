c0b197254a9747d07cc3370c203260b158aa9994
artze <arthurwtm@gmail.com>
2018-11-19 22:35:28 +0100

    Add footer styling

--- src/components/home/contact/Contact.vue
+++ src/components/home/contact/Contact.vue
@@ -2,7 +2,7 @@
     <v-layout justify-center align-end fill-height class="contact-section">
         <v-flex xs10 sm10 md10 lg10>
             <p class="contact-text">
-                Contact me on <a href="https://www.linkedin.com/in/arthur-wong/">Linkedin</a>
+                Contact me on <a href="https://www.linkedin.com/in/arthur-wong/" target="_blank">Linkedin</a>
             </p>
         </v-flex>
     </v-layout>
@@ -10,12 +10,18 @@
 
 <style scoped>
 .contact-section {
-    height: 400px;
+    height: 300px;
+    margin-top: 200px;
+    background-color: #d4d4d4;
 }
 
 .contact-text {
     font-family: 'Roboto';
     font-size: 2.5em;
-    font-weight: 300;
+    font-weight: 400;
+}
+
+.contact-text a {
+    color: #c52318;
 }
 </style>
