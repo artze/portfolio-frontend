162cf2877c23ab3c0c56cf433895ca3a3420a5c0
artze <arthurwtm@gmail.com>
2019-08-19 21:30:06 +0800

    Edit about text

--- src/components/home/about/About.vue
+++ src/components/home/about/About.vue
@@ -2,7 +2,7 @@
     <v-layout justify-center align-center fill-height class="about-section">
         <v-flex xs8 sm8 md8 lg8>
             <p class="about-text">
-                I started off managing finance &amp; operations, and transitioned into software development to address various operational inefficiencies. I am now a full-time developer building and maintaining an IoT platform, with a diverse skill-set and cross-functional work experience.
+                I am driven by the desire to design and build things. I have built web applications for various purposes, be it to streamline operational processes, to serve the needs of clients, or for myself. At the core, I'm a developer but I enjoy being involved in multiple areas of product development and see the results of my work in action. I am easily inspired to learn new things and to challenge myself, and always on the lookout for interesting projects.
             </p>
         </v-flex>
     </v-layout>
@@ -16,20 +16,18 @@
 .about-text {
     font-family: 'Roboto';
     font-size: 2.5em;
-    font-weight: 300;
+    font-weight: 200;
 }
 
 @media only screen and (max-width : 600px) {
     .about-text {
         font-size: 1.5em;
-        font-weight: 300;
     }
 }
 
 @media only screen and (min-width : 600px) and (max-width : 960px) {
     .about-text {
         font-size: 1.5em;
-        font-weight: 300;
     }
 }
 </style>
